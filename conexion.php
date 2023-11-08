24cf8f97f5a4915c92d66185591024d371607892
Julio Vazquez <juliovazquezm01@gmail.com>
2023-11-08 14:39:16 -0600

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+
+$localhost = 'localhost:3306';
+$username = 'root';
+$password = '';
+$dbname = 'asistencia';
+
+$conn = mysqli_connect($localhost, $username, $password, $dbname);
+	
+if (!$conn) {
+    die("Error en la conexión: " . mysqli_connect_error());
+} else{
+    echo "Conexion establecida";
+}
+
+?>
